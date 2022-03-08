92955d75ddf751449c5ce0c9cc4f96b723b752ea
Nguyen Trong Ngoc <trongngocthanhchuong2001@gmail.com>
2022-03-08 20:45:37 +0700

    form khach hang, co 4 nut

---
+++ Form_khach_hang/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Form_khach_hang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnWatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(57, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã khách hàng";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Aqua;
+            this.label2.Location = new System.Drawing.Point(57, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên khách hàng";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Aqua;
+            this.label3.Location = new System.Drawing.Point(57, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Địa chỉ";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Aqua;
+            this.label4.Location = new System.Drawing.Point(57, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số điện thoại";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.ForeColor = System.Drawing.Color.Aqua;
+            this.txtCode.Location = new System.Drawing.Point(289, 53);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(499, 30);
+            this.txtCode.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.ForeColor = System.Drawing.Color.Aqua;
+            this.txtName.Location = new System.Drawing.Point(289, 120);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(499, 30);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.ForeColor = System.Drawing.Color.Aqua;
+            this.txtAddress.Location = new System.Drawing.Point(289, 186);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(499, 30);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.ForeColor = System.Drawing.Color.Aqua;
+            this.txtNumber.Location = new System.Drawing.Point(289, 248);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(499, 30);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Aqua;
+            this.btnAdd.Location = new System.Drawing.Point(2, 331);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(145, 50);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.Aqua;
+            this.btnDelete.Location = new System.Drawing.Point(178, 331);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(145, 50);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Aqua;
+            this.btnExit.Location = new System.Drawing.Point(706, 331);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(145, 50);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecute.ForeColor = System.Drawing.Color.Aqua;
+            this.btnExecute.Location = new System.Drawing.Point(354, 331);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(145, 50);
+            this.btnExecute.TabIndex = 7;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnWatch
+            // 
+            this.btnWatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWatch.ForeColor = System.Drawing.Color.Aqua;
+            this.btnWatch.Location = new System.Drawing.Point(530, 331);
+            this.btnWatch.Name = "btnWatch";
+            this.btnWatch.Size = new System.Drawing.Size(145, 50);
+            this.btnWatch.TabIndex = 8;
+            this.btnWatch.Text = "Xem";
+            this.btnWatch.UseVisualStyleBackColor = true;
+            this.btnWatch.Click += new System.EventHandler(this.btnWatch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(857, 427);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnWatch);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnWatch;
+    }
+}
+
---
+++ Form_khach_hang/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_khach_hang
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection cnn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string s;
+            SqlCommand cmd = new SqlCommand();
+            s = "insert into khachhang (makh, TenKh, DCKH,DTKH)";
+            s += "values(@makh,@tenkh,@dckh,@dtkh)";
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = s;
+            cmd.Parameters.Add("@makh", SqlDbType.Float).Value = float.Parse(txtCode.Text);
+            cmd.Parameters.Add("@tenkh", SqlDbType.VarChar).Value = txtName.Text;
+            cmd.Parameters.Add("@dckh", SqlDbType.VarChar).Value = txtAddress.Text;
+            cmd.Parameters.Add("@dtkh", SqlDbType.VarChar).Value = txtNumber.Text;
+            cmd.CommandTimeout = 60;
+            cmd.ExecuteNonQuery();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string s;
+            SqlCommand cmd = new SqlCommand();
+            s = "delete from KhachHang where makh = @makh";
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = s;
+            cmd.Parameters.Add("@makh", SqlDbType.Float).Value = float.Parse(txtCode.Text);
+            cmd.CommandTimeout = 60;
+            cmd.ExecuteNonQuery();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select count(*) from KhachHang";
+            cmd.CommandTimeout = 60;
+            cmd.Connection = cnn;
+            int i = (int)cmd.ExecuteScalar();
+            MessageBox.Show(i.ToString());
+        }
+
+        private void btnWatch_Click(object sender, EventArgs e)
+        {
+            string s;
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from KhachHang";
+            cmd.CommandTimeout = 60;
+            cmd.Connection = cnn;
+            SqlDataReader rs;
+            rs = cmd.ExecuteReader();
+            int i;
+            i = rs.FieldCount;
+            s= rs.GetDataTypeName(1).ToString() + " - " + rs.GetName(1) + "= \n";
+            while(rs.Read())
+            {
+                for (i=0;i<rs.FieldCount;i++)
+                {
+                    s += " " + rs.GetValue(i);
+                }
+                s += "\n";
+            }
+            MessageBox.Show(s);
+            rs.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
