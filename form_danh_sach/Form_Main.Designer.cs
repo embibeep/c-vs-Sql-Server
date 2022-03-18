1ff6f9c4cef15b5b8abad0b7b3b966aff7dd0515
Nguyen Trong Ngoc <trongngocthanhchuong2001@gmail.com>
2022-03-18 20:33:15 +0700

    form 3

---
+++ form_danh_sach/Form_Main.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace form_danh_sach
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_MaNV = new System.Windows.Forms.TextBox();
+            this.txt_Ho = new System.Windows.Forms.TextBox();
+            this.txt_Ten = new System.Windows.Forms.TextBox();
+            this.txt_GioiTinh = new System.Windows.Forms.TextBox();
+            this.txt_LuongCB = new System.Windows.Forms.TextBox();
+            this.txt_CongViec = new System.Windows.Forms.TextBox();
+            this.txt_MaKV = new System.Windows.Forms.TextBox();
+            this.dgv_Staff = new System.Windows.Forms.DataGridView();
+            this.btn_Edit_Dgv = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Staff)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã NV";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(352, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Công Việc";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Tên";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(352, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Lương CB";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(352, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Nữ";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(696, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Mã KV";
+            // 
+            // txt_MaNV
+            // 
+            this.txt_MaNV.Location = new System.Drawing.Point(120, 13);
+            this.txt_MaNV.Name = "txt_MaNV";
+            this.txt_MaNV.Size = new System.Drawing.Size(212, 22);
+            this.txt_MaNV.TabIndex = 4;
+            // 
+            // txt_Ho
+            // 
+            this.txt_Ho.Location = new System.Drawing.Point(120, 64);
+            this.txt_Ho.Name = "txt_Ho";
+            this.txt_Ho.Size = new System.Drawing.Size(212, 22);
+            this.txt_Ho.TabIndex = 5;
+            // 
+            // txt_Ten
+            // 
+            this.txt_Ten.Location = new System.Drawing.Point(118, 120);
+            this.txt_Ten.Name = "txt_Ten";
+            this.txt_Ten.Size = new System.Drawing.Size(212, 22);
+            this.txt_Ten.TabIndex = 6;
+            // 
+            // txt_GioiTinh
+            // 
+            this.txt_GioiTinh.Location = new System.Drawing.Point(458, 12);
+            this.txt_GioiTinh.Name = "txt_GioiTinh";
+            this.txt_GioiTinh.Size = new System.Drawing.Size(212, 22);
+            this.txt_GioiTinh.TabIndex = 7;
+            // 
+            // txt_LuongCB
+            // 
+            this.txt_LuongCB.Location = new System.Drawing.Point(458, 64);
+            this.txt_LuongCB.Name = "txt_LuongCB";
+            this.txt_LuongCB.Size = new System.Drawing.Size(212, 22);
+            this.txt_LuongCB.TabIndex = 8;
+            // 
+            // txt_CongViec
+            // 
+            this.txt_CongViec.Location = new System.Drawing.Point(458, 120);
+            this.txt_CongViec.Name = "txt_CongViec";
+            this.txt_CongViec.Size = new System.Drawing.Size(212, 22);
+            this.txt_CongViec.TabIndex = 9;
+            // 
+            // txt_MaKV
+            // 
+            this.txt_MaKV.Location = new System.Drawing.Point(785, 11);
+            this.txt_MaKV.Name = "txt_MaKV";
+            this.txt_MaKV.Size = new System.Drawing.Size(212, 22);
+            this.txt_MaKV.TabIndex = 10;
+            // 
+            // dgv_Staff
+            // 
+            this.dgv_Staff.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Staff.Location = new System.Drawing.Point(17, 156);
+            this.dgv_Staff.Name = "dgv_Staff";
+            this.dgv_Staff.RowHeadersWidth = 51;
+            this.dgv_Staff.RowTemplate.Height = 24;
+            this.dgv_Staff.Size = new System.Drawing.Size(989, 350);
+            this.dgv_Staff.TabIndex = 11;
+            this.dgv_Staff.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Staff_CellClick);
+            // 
+            // btn_Edit_Dgv
+            // 
+            this.btn_Edit_Dgv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Edit_Dgv.Location = new System.Drawing.Point(17, 536);
+            this.btn_Edit_Dgv.Name = "btn_Edit_Dgv";
+            this.btn_Edit_Dgv.Size = new System.Drawing.Size(116, 40);
+            this.btn_Edit_Dgv.TabIndex = 12;
+            this.btn_Edit_Dgv.Text = "Sửa dgv";
+            this.btn_Edit_Dgv.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Update
+            // 
+            this.Btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Update.Location = new System.Drawing.Point(225, 536);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(116, 40);
+            this.Btn_Update.TabIndex = 13;
+            this.Btn_Update.Text = "Cập nhật";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.Location = new System.Drawing.Point(442, 536);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(116, 40);
+            this.btn_Add.TabIndex = 14;
+            this.btn_Add.Text = "Thêm";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(680, 536);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(116, 40);
+            this.btn_Delete.TabIndex = 15;
+            this.btn_Delete.Text = "Xóa";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Edit.Location = new System.Drawing.Point(890, 536);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(116, 40);
+            this.btn_Edit.TabIndex = 16;
+            this.btn_Edit.Text = "Sửa";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 588);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.Btn_Update);
+            this.Controls.Add(this.btn_Edit_Dgv);
+            this.Controls.Add(this.dgv_Staff);
+            this.Controls.Add(this.txt_MaKV);
+            this.Controls.Add(this.txt_CongViec);
+            this.Controls.Add(this.txt_LuongCB);
+            this.Controls.Add(this.txt_GioiTinh);
+            this.Controls.Add(this.txt_Ten);
+            this.Controls.Add(this.txt_Ho);
+            this.Controls.Add(this.txt_MaNV);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form danh sách";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Staff)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_MaNV;
+        private System.Windows.Forms.TextBox txt_Ho;
+        private System.Windows.Forms.TextBox txt_Ten;
+        private System.Windows.Forms.TextBox txt_GioiTinh;
+        private System.Windows.Forms.TextBox txt_LuongCB;
+        private System.Windows.Forms.TextBox txt_CongViec;
+        private System.Windows.Forms.TextBox txt_MaKV;
+        private System.Windows.Forms.DataGridView dgv_Staff;
+        private System.Windows.Forms.Button btn_Edit_Dgv;
+        private System.Windows.Forms.Button Btn_Update;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Edit;
+    }
+}
+
---
+++ form_danh_sach/Form_Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_danh_sach
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Connection()
+        {
+            //Kết nối database vào qua hàm Connection.
+            //Tạo database tên STUDENT, tạo  table Student có chưa các thuộc tính cần thiết.
+            SqlConnection Connection = new SqlConnection("server=.;database=STUDENT;integrated security=true");
+            SqlCommand cmd = new SqlCommand("Select * From Student", Connection);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            BindingSource binding = new BindingSource();
+            binding.DataSource = dt;
+            dgv_Staff.DataSource = binding;
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            //load data ngay khi chạy chương trình.
+            Connection();
+        }
+
+
+        private void btn_Edit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_Staff_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            if (index >= 0)
+            {
+                //các thuộc tính trong ngoặc vuông có thể thay đổi tùy theo tên biến trong database.
+                txt_MaNV.Text = dgv_Staff.CurrentRow.Cells["MaNV"].Value.ToString();
+                txt_Ho.Text = dgv_Staff.CurrentRow.Cells["Ho"].Value.ToString();
+                txt_Ten.Text = dgv_Staff.CurrentRow.Cells["Ten"].Value.ToString();
+                txt_GioiTinh.Text = dgv_Staff.CurrentRow.Cells["GioiTinh"].Value.ToString();
+                txt_LuongCB.Text = dgv_Staff.CurrentRow.Cells["LuongCB"].Value.ToString();
+                txt_CongViec.Text = dgv_Staff.CurrentRow.Cells["CongViec"].Value.ToString();
+                txt_MaKV.Text = dgv_Staff.CurrentRow.Cells["MaKV"].Value.ToString();
+            }
+        }
+    }
+}
