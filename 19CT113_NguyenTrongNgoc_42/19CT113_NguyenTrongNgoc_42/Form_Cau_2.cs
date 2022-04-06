8d7983c8365b07e27a0851ec8a91cb5b3b2f1567
Nguyễn Trọng Ngọc <71998734+embibeep@users.noreply.github.com>
2022-04-06 09:44:22 +0700

    add form kiemtra 2

---
+++ 19CT113_NguyenTrongNgoc_42/19CT113_NguyenTrongNgoc_42/Form_Cau_2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19CT113_NguyenTrongNgoc_42
+{
+    public partial class Form_Cau_2 : Form
+    {
+        public Form_Cau_2()
+        {
+            InitializeComponent();
+        }
+
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataTable dt = new DataTable();
+        BindingSource binding = new BindingSource();
+        SqlCommandBuilder cmb;
+
+        private void Connection()
+        {
+            //Kết nối database vào qua hàm Connection.
+            //Tạo database tên STUDENT, tạo  table Student có chưa các thuộc tính cần thiết.
+            SqlConnection Connection = new SqlConnection("server=.;database=TheThao;integrated security=true");
+            cmd = new SqlCommand("Select * From CLB", Connection);
+            da = new SqlDataAdapter(cmd);
+            //DataTable dt;
+            da.Fill(dt);
+            binding.DataSource = dt;
+            dgv_Staff_2.DataSource = binding;
+        }
+
+        private void Form_Cau_2_Load(object sender, EventArgs e)
+        {
+            Connection();
+        }
+
+        private void dgv_Staff_2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            if (index >= 0)
+            {
+                //các thuộc tính trong ngoặc vuông có thể thay đổi tùy theo tên biến trong database.
+                txt_MaCLB.Text = dgv_Staff_2.CurrentRow.Cells["MaCLB"].Value.ToString();
+                txt_TenCLB.Text = dgv_Staff_2.CurrentRow.Cells["TenCLB"].Value.ToString();
+                txt_SoDoi.Text = dgv_Staff_2.CurrentRow.Cells["SoDoi"].Value.ToString();
+            }
+        }
+
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            DataTable tbl = new DataTable();
+            tbl = dt.GetChanges();
+            if (tbl == null)
+            {
+                MessageBox.Show("Cập nhật thất bại!");
+            }
+            else
+            {
+                cmb = new SqlCommandBuilder(da);
+                da.Update(dt);
+                MessageBox.Show("Có" + tbl.Rows.Count + " danh sách đã được cập nhật!");
+            }
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            SqlConnection Connection = new SqlConnection("server=.;database=TheThao;integrated security=true");
+            string Scon;
+            Connection.Open();
+            Scon = "insert into CLB values(@MaCLB,@TenCLB,@SoDoi)";
+            SqlCommand cmd1 = new SqlCommand(Scon, Connection);
+            cmd1.Parameters.Add("@MaCLB", SqlDbType.NVarChar).Value = txt_MaCLB.Text;
+            cmd1.Parameters.Add("@TenCLB", SqlDbType.NVarChar).Value = txt_TenCLB.Text;
+            cmd1.Parameters.Add("@SoDoi", SqlDbType.NVarChar).Value = txt_SoDoi.Text;
+
+            int count = cmd1.ExecuteNonQuery();
+            {
+                MessageBox.Show("Thêm thành công!");
+            }
+            Connection.Close();
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection Connection = new SqlConnection("server=.;database=TheThao;integrated security=true");
+            string SCon;
+            SCon = "Delete From CLB Where MaCLB = @MaCLB";
+            SqlCommand cmd1 = new SqlCommand(SCon, Connection);
+            Connection.Open();
+            cmd1.Parameters.Add("@MaCLB", SqlDbType.NVarChar).Value = txt_MaCLB.Text;
+            int count = cmd1.ExecuteNonQuery();
+            if (count > 0)
+            {
+                DataRowView row = (DataRowView)binding.Current;
+                row.Delete();
+                MessageBox.Show("Xóa thành công!");
+            }
+            Connection.Close();
+        }
+
+        private void btn_Edit_Dgv_Click(object sender, EventArgs e)
+        {
+            dt.Clear();
+            da.Fill(dt);
+        }
+
+
+    }
+}
---
+++ 19CT113_NguyenTrongNgoc_42/19CT113_NguyenTrongNgoc_42/Form_Main.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace _19CT113_NguyenTrongNgoc_42
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Cau_1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Cau_1
+            // 
+            this.btn_Cau_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cau_1.Location = new System.Drawing.Point(236, 89);
+            this.btn_Cau_1.Name = "btn_Cau_1";
+            this.btn_Cau_1.Size = new System.Drawing.Size(120, 48);
+            this.btn_Cau_1.TabIndex = 0;
+            this.btn_Cau_1.Text = "Câu 1";
+            this.btn_Cau_1.UseVisualStyleBackColor = true;
+            this.btn_Cau_1.Click += new System.EventHandler(this.btn_Cau_1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(236, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Câu 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Location = new System.Drawing.Point(236, 267);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(120, 48);
+            this.btn_Exit.TabIndex = 2;
+            this.btn_Exit.Text = "Thoát";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CƠ SỞ DỮ LIỆU";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 409);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Cau_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Cau_1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
