b431f8f16f788e2c432b1862069bbaf4f037adb0
Nguyễn Trọng Ngọc <71998734+embibeep@users.noreply.github.com>
2022-04-13 10:32:17 +0700

    add new form

---
+++ form_luoi_danh_sach/19ct113_12_NguyenHoaiNam/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace _19ct113_12_NguyenHoaiNam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtdatabase = new System.Windows.Forms.TextBox();
+            this.txtserver = new System.Windows.Forms.TextBox();
+            this.rdsql = new System.Windows.Forms.RadioButton();
+            this.rdwin = new System.Windows.Forms.RadioButton();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Mật khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Tài khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Tên Database";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Tên Máy Chủ";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(171, 264);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(214, 20);
+            this.txtpass.TabIndex = 21;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(171, 224);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(214, 20);
+            this.txtuser.TabIndex = 20;
+            // 
+            // txtdatabase
+            // 
+            this.txtdatabase.Location = new System.Drawing.Point(171, 125);
+            this.txtdatabase.Name = "txtdatabase";
+            this.txtdatabase.Size = new System.Drawing.Size(214, 20);
+            this.txtdatabase.TabIndex = 19;
+            this.txtdatabase.Text = "HoaDon";
+            // 
+            // txtserver
+            // 
+            this.txtserver.Location = new System.Drawing.Point(171, 88);
+            this.txtserver.Name = "txtserver";
+            this.txtserver.Size = new System.Drawing.Size(214, 20);
+            this.txtserver.TabIndex = 18;
+            this.txtserver.Text = ".";
+            // 
+            // rdsql
+            // 
+            this.rdsql.AutoSize = true;
+            this.rdsql.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdsql.Location = new System.Drawing.Point(185, 187);
+            this.rdsql.Name = "rdsql";
+            this.rdsql.Size = new System.Drawing.Size(164, 19);
+            this.rdsql.TabIndex = 17;
+            this.rdsql.Text = "Chứng thực bằng SQL";
+            this.rdsql.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.rdsql.UseVisualStyleBackColor = true;
+            this.rdsql.CheckedChanged += new System.EventHandler(this.rdsql_CheckedChanged);
+            // 
+            // rdwin
+            // 
+            this.rdwin.AutoSize = true;
+            this.rdwin.Checked = true;
+            this.rdwin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdwin.Location = new System.Drawing.Point(185, 164);
+            this.rdwin.Name = "rdwin";
+            this.rdwin.Size = new System.Drawing.Size(187, 19);
+            this.rdwin.TabIndex = 16;
+            this.rdwin.TabStop = true;
+            this.rdwin.Text = "Chứng thực bằng Window";
+            this.rdwin.UseVisualStyleBackColor = true;
+            this.rdwin.CheckedChanged += new System.EventHandler(this.rdwin_CheckedChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(295, 307);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(175, 307);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 14;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(425, 37);
+            this.panel1.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(425, 37);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "KẾT NỐI CƠ SỞ DỮ LIỆU";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 377);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.txtdatabase);
+            this.Controls.Add(this.txtserver);
+            this.Controls.Add(this.rdsql);
+            this.Controls.Add(this.rdwin);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtdatabase;
+        private System.Windows.Forms.TextBox txtserver;
+        private System.Windows.Forms.RadioButton rdsql;
+        private System.Windows.Forms.RadioButton rdwin;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ form_luoi_danh_sach/19ct113_12_NguyenHoaiNam/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19ct113_12_NguyenHoaiNam
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn = new SqlConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            txtuser.Enabled = false;
+            txtpass.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string connectionstring;
+
+
+            try
+            {
+                if (rdwin.Checked == true)
+                {
+                    connectionstring = "Server=" + txtserver.Text;
+                    connectionstring += ";database=" + txtdatabase.Text;
+                    connectionstring += ";integrated security=true";
+                    cnn.ConnectionString = connectionstring;
+                }
+                else
+                {
+                    connectionstring = "Server=" + txtserver.Text;
+                    connectionstring += ";database=" + txtdatabase.Text;
+                    connectionstring += ";uid=" + txtuser.Text;
+                    connectionstring += ";pwd=" + txtpass.Text;
+                    cnn.ConnectionString = connectionstring;
+                }
+                cnn.Open();
+                MessageBox.Show("ket noi thanh cong");
+
+                this.Hide();
+                ThongTin view = new ThongTin(cnn);
+                view.ShowDialog();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("loi ke noi" + ex.Message);
+            }
+        }
+        
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn đóng chương trình", "Exit", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void rdwin_CheckedChanged(object sender, EventArgs e)
+        {
+            txtuser.Enabled = false;
+            txtpass.Enabled = false;
+            
+        }
+
+        private void rdsql_CheckedChanged(object sender, EventArgs e)
+        {
+            txtuser.Enabled = true;
+            txtpass.Enabled = true;
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
